dec1eb3e64e37909674671fd49f73178ce654318
Алексей Ермолаев <asvaltgovno@gmail.com>
2023-03-05 14:24:19 +0300

    profile и photo осталось по функционалу

--- app/src/main/java/com/example/up/Login.java
+++ app/src/main/java/com/example/up/Login.java
@@ -3,7 +3,9 @@ package com.example.up;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,68 +23,83 @@ import java.net.URL;
 
 public class Login extends AppCompatActivity {
 
-    TextInputLayout email;
-    TextInputLayout password;
+    TextInputLayout emailLayout;
+    TextInputLayout passwordLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
+        emailLayout = findViewById(R.id.email);
+        passwordLayout = findViewById(R.id.password);
     }
     @SuppressLint("NotConstructor")
     public void Login(View v){
-        if(email.getEditText().getText().toString().contains("@")){
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        JSONObject postData = new JSONObject();
-                        postData.put("email", email.getEditText().getText());
-                        postData.put("password", password.getEditText().getText());
+        if(emailLayout.getEditText().getText().toString().contains("@")){
+           getUser(emailLayout.getEditText().getText().toString(),passwordLayout.getEditText().getText().toString());
+        }
+    }
 
-                        // создаем соединение
-                        URL url = new URL("http://mskko2021.mad.hakta.pro/api/user/login");
-                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                        conn.setDoOutput(true);
-                        conn.setRequestMethod("POST");
-                        conn.setRequestProperty("Content-Type", "application/json");
-                        // отправляем данные на сервер
-                        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-                        writer.write(postData.toString());
-                        writer.flush();
-                        // получаем ответ от сервера
-                        StringBuilder sb = new StringBuilder();
-                        int HttpResult = conn.getResponseCode();
-                        if (HttpResult == HttpURLConnection.HTTP_OK) {
-                            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
-                            String line = null;
-                            while ((line = br.readLine()) != null) {
-                                sb.append(line + "\n");
-                            }
-                            br.close();
-                            JSONObject response = new JSONObject(sb.toString());
-                            User.Id = response.getString("id");
-                            User.avatar = response.getString("avatar");
-                            User.nickName = response.getString("nickName");
-                            User.email = response.getString("email");
-                            User.token = response.getString("token");
-                            Log.d("response",response.toString());
+    void getUser(String email,String password){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONObject postData = new JSONObject();
+                    postData.put("email", email);
+                    postData.put("password", password);
 
-                            Intent main = new Intent(Login.this,MainActivity.class);
-                            startActivity(main);
-                        } else {
-                            Toast.makeText(Login.this, "invalid username or password", Toast.LENGTH_SHORT).show();
+                    // создаем соединение
+                    URL url = new URL("http://mskko2021.mad.hakta.pro/api/user/login");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoOutput(true);
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    // отправляем данные на сервер
+                    OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+                    writer.write(postData.toString());
+                    writer.flush();
+                    // получаем ответ от сервера
+                    StringBuilder sb = new StringBuilder();
+                    int HttpResult = conn.getResponseCode();
+                    if (HttpResult == HttpURLConnection.HTTP_OK) {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+                        String line = null;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line + "\n");
                         }
-                    }
-                    catch (Exception e){
-                        Log.d("e",e.toString());
+                        br.close();
+                        JSONObject response = new JSONObject(sb.toString());
+
+                        // Получаем объект SharedPreferences
+                        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+                        // Получаем объект Editor для редактирования SharedPreferences
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        // Сохраняем данные о пользователе
+                        editor.putString("password", String.valueOf(password));
+                        editor.putString("email", String.valueOf(email));
+                        // Применяем изменения
+                        editor.apply();
+
+                        User.Id = response.getString("id");
+                        User.avatar = response.getString("avatar");
+                        User.nickName = response.getString("nickName");
+                        User.email = response.getString("email");
+                        User.token = response.getString("token");
+                        Log.d("response",response.toString());
+
+                        Intent main = new Intent(Login.this,MainActivity.class);
+                        startActivity(main);
+                    } else {
+                        Toast.makeText(Login.this, "invalid username or password", Toast.LENGTH_SHORT).show();
                     }
                 }
-            }).start();
-        }
+                catch (Exception e){
+                    Log.d("e",e.toString());
+                }
+            }
+        }).start();
     }
 
     public void GoReg(View v){
--- app/src/main/java/com/example/up/MainActivity.java
+++ app/src/main/java/com/example/up/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +30,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    ListView listView;
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ImageView image = findViewById(R.id.image);
         recyclerView = findViewById(R.id.my_recycler_view);
+        listView = findViewById(R.id.QuotesList);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL); // Установка ориентации горизонтальной
         recyclerView.setLayoutManager(layoutManager);
@@ -44,6 +48,55 @@ public class MainActivity extends AppCompatActivity {
         TextView w_name = findViewById(R.id.welcome_name);
         w_name.setText("С возвращением, " + User.nickName);
         getFeelings();
+        getQuotes();
+    }
+
+    public void getQuotes() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // создаем соединение
+                    URL url = new URL("http://mskko2021.mad.hakta.pro/api/quotes");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    int responseCode = conn.getResponseCode();
+                    // Чтение ответа сервера
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        StringBuilder sb = new StringBuilder();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+                        String line = null;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line + "\n");
+                        }
+                        br.close();
+                        JSONObject response = new JSONObject(sb.toString());
+                        // Обработка ответа сервера
+                        List<Quote> quoteList = new ArrayList<Quote>();
+                        JSONArray json_array = new JSONArray(response.getString("data"));
+                        for (int i = 0; i < json_array.length(); i++) {
+                            JSONObject json_object = json_array.getJSONObject(i);
+                            Quote quote = new Quote();
+                            quote.image = json_object.getString("image");
+                            quote.title = json_object.getString("title");
+                            quote.description = json_object.getString("description");
+                            quoteList.add(quote);
+                        }
+                        //Заполняем список
+                        LVAdapter adapter = new LVAdapter(MainActivity.this,quoteList);
+                        listView.setAdapter(adapter);
+
+                    } else {
+                        Log.e("TAG", "Error: " + responseCode);
+                    }
+
+                    conn.disconnect();
+                }
+                catch (Exception e){
+                    Log.d("e",e.toString());
+                }
+            }
+        }).start();
     }
 
     public void getFeelings() {
